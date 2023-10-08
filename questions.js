bcaa1d16289365503c5167e0ff39e29acaa3110f
GuilhermeOLemos <142642797+GuilhermeOLemos@users.noreply.github.com>
2023-10-08 20:24:38 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,126 @@
+export default [
+
+    {
+        question: "Quando ocorre uma lua de sangue?",
+        answers: [
+            { option: "Num eclipse lunar", correct: false },
+            { option: "Num eclipse solar", correct: false },
+            { option: "Nenhuma das anteriores", correct: true },
+        ],
+    },
+
+    {
+        question: "Qual opção corresponde a um fator que diferencia um eclipse parcial de um eclipse total?",
+        answers: [
+            { option: "A posição do Sol", correct: false },
+            { option: "A posição da Lua", correct: false },
+            { option: "A posição da Terra", correct: false },
+            { option: "Todas as alternativas anteriores", correct: true },
+        ],
+    },
+
+    {
+        question: "Qual a definição de um eclipse?",
+        answers: [
+            { option: "Obscurecimento total ou parcial de um astro por outro", correct: true },
+            { option: "Quando o Sol é coberto pela Lua", correct: false },
+            { option: "Quando o sol é coberto pela Terra na visão da Lua", correct: false },
+        ],
+    },
+
+    {
+        question: "Qual a quantidade mínima de corpos celestes é necessária para se fazer um eclipse?",
+        answers: [
+            { option: "Menos que 3", correct: false },
+            { option: "Mais que 3", correct: false },
+            { option: "3 ou mais", correct: true },
+        ],
+    },
+
+    {
+        question: "O que é um eclipse solar?",
+        answers: [
+            { option: "É um fenômeno que ocorre quando a Lua se interpõe entre a Terra e o Sol", correct: true },
+            { option: "É um fenômeno que ocorre quando o Sol se interpõe entre a Lua e a Terra", correct: false },
+            { option: "É um fenômeno que ocorre quando a Terra se interpõe entre o Sol e a Lua", correct: false },
+        ],
+    },
+
+    {
+        question: "O que poderia acontecer com a nossa visão se observássemos um eclipse solar utilizando um óculos de sol?",
+        answers: [
+            { option: "Não seria prejudicada, pois o material que compõe os óculos escuros oferece proteção UV, impedindo a passagem de luz", correct: false },
+            { option: "Seria prejudicada, pois somente é bloqueada uma parte da luz, e os raios ultravioleta ainda vão estar passando", correct: true },
+        ],
+    },
+
+    {
+        question: "Qual o nome dado aos pontos onde a órbita da Lua cruza a eclíptica?",
+        answers: [
+            { option: "Saros", correct: false },
+            { option: "Nodo lunar", correct: true },
+            { option: "Penumbra", correct: false },
+        ],
+    },
+
+    {
+        question: "Por que vemos um eclipse parcial?",
+        answers: [
+            { option: "Pois a projeção da sombra da Lua sobre a Terra está na parte da Penumbra, enquanto a projeção da umbra está do lado de fora", correct: true },
+            { option: "Pois a projeção da sombra da Lua sobre a Terra está na parte da umbra, enquanto a projeção da penumbra está do lado de fora", correct: false },
+        ],
+    },
+
+    {
+        question: "Com que frequência acontece um eclipse lunar?",
+        answers: [
+            { option: "Somente um", correct: false },
+            { option: "No mínimo, três por ano", correct: false },
+            { option: "No mínimo, dois por ano", correct: true },
+        ],
+    },
+
+    {
+        question: "Qual tipo de eclipse podemos olhar a olho nu?",
+        answers: [
+            { option: "Solar", correct: false },
+            { option: "Lunar", correct: true },
+        ],
+    },
+
+    {
+        question: "Qual a ordem correta do eclipse lunar?",
+        answers: [
+            { option: "Terra, Lua e Sol", correct: false },
+            { option: "Lua, Sol e Terra", correct: false },
+            { option: "Sol, Terra e Lua", correct: true },
+        ],
+    },
+
+    {
+        question: "Qual a duração mínima e máxima de um eclipse lunar?",
+        answers: [
+            { option: "3 e 8", correct: true },
+            { option: "4 e 9", correct: false },
+            { option: "8 e 3", correct: false },
+        ],
+    },
+
+    {
+        question: "O que é uma região da penumbra?",
+        answers: [
+            { option: "É a região em que somente uma parte do corpo ocultante está obscurecendo a fonte de luz", correct: true },
+            { option: "É uma zona totalmente sem luz", correct: false },
+        ],
+    },
+
+    {
+        question: "Qual a região que tem a maior tendência a ter eclipses?",
+        answers: [
+            { option: "Norte", correct: false },
+            { option: "Sul", correct: false },
+            { option: "Chance igual", correct: true },
+        ],
+    },
+
+]
